f1731a62d37214371e33cf2f256954658d393250
Sz-Tim <tim.szewczyk@gmail.com>
2021-02-19 15:07:51 +0100

    enforcing scalar to avoid warnings

--- code/00_fn.R
+++ code/00_fn.R
@@ -17,7 +17,7 @@
 intersect_with_composition <- function(ant_buffer, landcover, category_col) {
   library(sf); library(tidyverse)
   st_intersection(ant_buffer %>% select(TubeNo), 
-                  landcover %>% select(first(category_col))) %>% 
+                  landcover %>% select(category_col[1])) %>% 
     mutate(area=st_area(.)) %>% group_by(TubeNo) %>%
     mutate(prop=as.numeric(area/sum(area)))
 }
@@ -44,8 +44,8 @@ categorize_buffers <- function(intsct.df, category_col, col_suffix) {
   library(sf); library(tidyverse)
   full_join(intsct.df %>% st_set_geometry(NULL) %>%
               group_by(TubeNo) %>% slice_max(prop) %>% ungroup %>%
-              select(TubeNo, first(category_col), prop) %>%
-              rename(cat_=category_col, prLargest_=prop) %>%
+              select(TubeNo, category_col[1], prop) %>%
+              rename(cat_=category_col[1], prLargest_=prop) %>%
               rename_with(., ~paste0(.x, col_suffix), 
                           one_of("cat_", "prLargest_")),
             intsct.df %>% st_set_geometry(NULL) %>% 
